bac9527e3932b81fe195a985c417f8f8616701e7
Thais Menezes <thais.mas@hotmail.com>
2024-12-17 17:14:45 -0300

    Initial Commit

---
+++ SwiftUIFirebaseBootcamp/SwiftUIFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseBootcampApp.swift
+//  SwiftUIFirebaseBootcamp
+//
+//  Created by Thais Souza on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
